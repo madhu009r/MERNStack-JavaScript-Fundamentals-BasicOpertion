c1ade45b4ae5d62240642d7dd3db2be439a10436
madhu009r <165657391+madhu009r@users.noreply.github.com>
2025-06-03 10:08:44 +0530

    Add files via upload

---
+++ javascript/js_index2.js
@@ -0,0 +1,94 @@
+
+
+
+// const city1="Cbo"
+// const city= new String("cbo")
+
+// console.log(typeof(city1))
+// console.log(typeof(city))
+// if(city1 === city){
+//     console.log('equal')
+// }
+// else{
+//     console.log('not Equal')
+// }
+
+
+
+let fruits = "Apple, Banana, Kiwi"
+let people = "Reshab, Ravi"
+let sliced = fruits.slice(7, 13)
+let subString = fruits.substring(7,13)
+const capital = fruits.toUpperCase()
+const concation = fruits.concat(" ", people)
+ 
+console.log(concation)
+console.log(capital)
+console.log(sliced)
+console.log(subString)
+ 
+ 
+let data = "    Hello I am     ";
+console.log(data.length);
+let trimmed = data.trim();
+console.log(trimmed.length);
+console.log(trimmed);
+ 
+let age = 27
+ 
+let myData = `Reshab and my age is ${age}`
+console.log(myData)
+ 
+ 
+ const firstName = "Reshab_Kumar"              
+const lastName = 'Shaw'
+const movie = "Marvel's \"Avengers\" End Game"
+// console.log(firstName.length)
+ 
+ 
+ 
+const city = new String("Vaishali")
+const city1 = "Vaishali"
+ 
+if(city1 === city) {
+    console.log('Equal')
+} else {
+    console.log("Not Equal")
+}
+ 
+console.log(typeof(city))
+console.log(typeof(city1))
+
+
+
+let data = "Hello        I am";
+console.log(data.length);
+let trimmed = data.trimStart();
+console.log(trimmed.length);
+console.log(trimmed);
+ let fruits = "Apple, Banana, Kiwi"
+let people = "Reshab, Ravi"
+let sliced = fruits.slice(7, 13)
+let subString = fruits.substring(7,13)
+const capital = fruits.toUpperCase()
+const concation = fruits.concat(" ", people)
+ 
+console.log(concation)
+console.log(capital)
+console.log(sliced)
+console.log(subString)
+ 
+ 
+let data = "    Hello I am     ";
+console.log(data.length);
+let trimmed = data.trim();
+console.log(trimmed.length);
+console.log(trimmed);
+ 
+let age = 27
+ 
+let myData = `Reshab and my age is ${age}`
+console.log(myData)
+ 
+ 
+ 
---
+++ javascript/js_index3.js
@@ -0,0 +1,116 @@
+let data = "Hello We are learning MERN Tech";
+let index = data.indexOf("MERN");
+let index1 = data.lastIndexOf("MERN")
+let index2 = data.lastIndexOf("Reshab")
+let index3 = data.indexOf("learning", 17)
+ console.log(index)
+  console.log(index1)
+console.log(index2)
+ console.log(index3)
+
+ let age="reshab"
+
+ let d= parseInt(age)
+ console.log(d)
+ 
+let data1 = "Hii We are Developers"
+let index4 = data1.search("are")
+let matchingString = data1.match("Developers")
+let paymentOptions = ["UPI", "CREDIT_CARD"];
+let matchOptions = paymentOptions.includes("NetBanking")
+ 
+// console.log(index4)
+// console.log(matchingString)
+console.log(matchOptions)
+
+
+
+// let data = "Hello We are learning MERN Tech";
+// let index = data.indexOf("MERN");
+// let index1 = data.lastIndexOf("MERN")
+// let index2 = data.lastIndexOf("Reshab")
+// let index3 = data.indexOf("learning", 17)
+// console.log(index)
+// console.log(index1)
+// console.log(index2)
+// console.log(index3)
+ 
+// let data1 = "Hii We are Developers"
+// let index4 = data1.search("are")
+// let matchingString = data1.match("Developers")
+// let paymentOptions = ["UPI", "CREDIT_CARD"];
+// let matchOptions = paymentOptions.includes("NetBanking")
+ 
+// // console.log(index4)
+// // console.log(matchingString)
+// console.log(matchOptions)
+ 
+ 
+//JavaScript Numbers
+ 
+// let x = 3
+// let y = 3.143
+// console.log(typeof(x))
+// console.log(typeof(y))
+ 
+// let x = 123e5;
+// let y = 123e-5
+//  console.log(typeof(x))
+// console.log(typeof(y))
+ 
+// let x = 0.3 + 0.2
+// String(x)
+// console.log(x)
+ 
+// let y = (0.3 * 5 + 0.2 * 5) / 10;
+// String(y)
+// console.log(y)
+ 
+// let z = x + y
+// console.log(z)
+ 
+ 
+// let a = '10'
+// let b = '20'
+// console.log(a+b)
+ 
+// let a = 100 / "10"
+// let b = isNaN(a)
+// console.log(typeof(a))
+// console.log(b)
+ 
+// let c = 100 / "reshab"
+// let e = "reshab" / 100;
+// let d = isNaN(c)
+// console.log(d)
+// console.log(c)
+// console.log(e)
+ 
+// let myNum = 10;
+ 
+// let data = ""
+// while (myNum != Infinity) {
+//     myNum = myNum * myNum;
+//     data = data + myNum + "<br>"
+// }
+ 
+// console.log(data)
+// console.log(typeof(Infinity))
+ 
+// let f = myNum.toString(8)
+// console.log(f)
+// let x = 8.7672;
+// let y = x.toExponential(2)
+// let z = x.toExponential(6)
+// let a = x.toFixed(3)
+// console.log(a)
+// console.log(y,z)
+ 
+let myAge = "Aakil"
+let myNum = 67
+let check = Number.isInteger(myNum)
+console.log(check)
+// let d = parseInt(myAge)
+// let f = parseFloat(myNum)
+// console.log(f)
+// console.log(typeof(f))
