66ac655535785f024515df536608f49bf00c4f63
madhu009r <165657391+madhu009r@users.noreply.github.com>
2025-06-04 09:13:55 +0530

    Add files via upload

---
+++ javascript/Array.js
@@ -0,0 +1,46 @@
+
+const fruits=new Array("apple","mango","Watermelon")
+
+// console.log(typeof(fruit))
+// console.log(fruit)
+
+// fruit.push("guava")
+// console.log(fruit)
+// fruit.pop()
+// console.log(fruit)
+
+// fruit.shift(fruit)
+// fruit.unshift("totmato")
+
+// console.log(fruit)
+
+
+
+// const vegetables = ["Potato", "Cauli Flower", "lady's finger", "Brinjal", "Drumstick"]
+// const recipe = ["Indian Curry", "Butter Chicken Masala", "Mutton biryani", "Curd Rice", "Pani Puri" ]
+ 
+// const myFavFood = vegetables.concat(fruits, recipe)
+// const myFavFood1 = fruits.concat(recipe,vegetables)
+ 
+// console.log(myFavFood)
+// console.log(myFavFood1)
+
+
+
+// fruits.slice(1,0,"guava")
+// console.log(fruits)
+
+
+
+// Splice & Slice
+ 
+const fruit = ["Apple", "banana", "Kiwi", "Pear"]
+fruit.splice(1,3,"Guava")
+console.log('1',fruit)
+ 
+// fruits.splice(0,2)
+// console.log('2',fruits)
+ 
+const data = fruit.slice(1)
+console.log(data)
+ 
